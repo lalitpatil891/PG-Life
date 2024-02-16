4bb246f49f583a76e47635b12c9a41506d3348fb
Lalit Patil <lp613408@gmail.com>
2024-02-16 21:45:20 +0530

    files added

---
+++ react-app/src/NoProperty_final.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoProperty = () => {
+
+  return (
+    <div className="no-property-container">
+      <p>No ResiComfort to list</p>
+    </div>
+  );
+}
+
+export default NoProperty;
